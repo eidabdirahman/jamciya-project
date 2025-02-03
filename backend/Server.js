63eac6e1f12eb6d2167ccd3dd6cdea8d34dfddb7
Eid Abdirahman <140173086+eidabdirahman@users.noreply.github.com>
2025-02-03 17:48:16 +0300

    cors updated in server.js file

--- backend/Server.js
+++ backend/Server.js
@@ -29,7 +29,10 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: 'https://jamciya-project.vercel.app',
+  credentials: true, 
+}));
 
 
 // Define __dirname in ES Module
