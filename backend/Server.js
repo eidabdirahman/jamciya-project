517d28f15e166d5aa6218ec3d9b8ccfbdcba3c6b
Eid Abdirahman <140173086+eidabdirahman@users.noreply.github.com>
2025-02-03 11:06:33 +0300

    cors in server.js updated

--- backend/Server.js
+++ backend/Server.js
@@ -28,10 +28,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-  origin: 'https://jamciya-project.vercel.app', // Replace with your frontend domain
-  credentials: true,
-}));
+app.use(cors());
 
 
 // Serve static files from the "uploads" directory
