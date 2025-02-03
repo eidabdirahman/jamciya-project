5bcfc396086524db2010958651bca3cd01ed6f37
Eid Abdirahman <140173086+eidabdirahman@users.noreply.github.com>
2025-02-03 17:14:33 +0300

    constants updated

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,9 @@
 export const BASE_URL = 'https://jamciya-project-serverside.vercel.app';
-export const BLOGS_URL = '/api/blogs';
-export const PROJECTS_URL = '/api/projects';
-export const PARTNERS_URL = '/api/partners';
-export const DEPARTMENTS_URL = '/api/departments';
-export const PHOTOGALLERY_URL = '/api/photogallery';
-export const VIDEOGALLERY_URL = '/api/videos';
-export const USERS_URL = '/api/users';
+
+export const BLOGS_URL = `${BASE_URL}/api/blogs`;
+export const PROJECTS_URL = `${BASE_URL}/api/projects`;
+export const PARTNERS_URL = `${BASE_URL}/api/partners`;
+export const DEPARTMENTS_URL = `${BASE_URL}/api/departments`;
+export const PHOTOGALLERY_URL = `${BASE_URL}/api/photogallery`;
+export const VIDEOGALLERY_URL = `${BASE_URL}/api/videos`;
+export const USERS_URL = `${BASE_URL}/api/users`;
